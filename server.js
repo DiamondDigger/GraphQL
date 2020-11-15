d9e8534915b902260f24ef4f98a43617f61bb0f2
naglec13 <naglec13@gmail.com>
2020-11-16 01:00:16 +0300

    added simple schema with data

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+const { request } = require('express')
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+const {buildSchema, GraphQLObjectType, GraphQLString, GraphQLSchema} = require('graphql')
 const app = express()
+
+// const schema = buildSchema(`
+//     type Query{
+//         hello: String
+//     }
+// `)
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name:'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello world from GraphQL'
+            }
+        })
+    })
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
 app.listen(5001, () => console.log('Server is running now...'))
