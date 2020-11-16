19f03c707118a2354ab73ed1fd2a05db5a94f077
naglec13 <naglec13@gmail.com>
2020-11-16 23:56:57 +0300

    added complex schema with mock data

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const { request } = require('express')
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
-const {buildSchema, GraphQLObjectType, GraphQLString, GraphQLSchema} = require('graphql')
+const {
+    buildSchema, 
+    GraphQLObjectType, 
+    GraphQLSchema, 
+    GraphQLNonNull, 
+    GraphQLList,
+    GraphQLInt,
+    GraphQLString} = require('graphql')
 const app = express()
 
 // const schema = buildSchema(`
@@ -10,20 +17,91 @@ const app = express()
 //     }
 // `)
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name:'HelloWorld',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello world from GraphQL'
-            }
-        })
+// const schema = new GraphQLSchema({
+//     query: new GraphQLObjectType({
+//         name:'HelloWorld',
+//         fields: () => ({
+//             message: {
+//                 type: GraphQLString,
+//                 resolve: () => 'Hello world from GraphQL'
+//             }
+//         })
+//     })
+// })
+
+const authors = [
+    {id: 1, name: 'J. K. Rowling'},
+    {id: 2, name: 'J. R. R. Tolkien'},
+    {id: 3, name: 'Brent Weeks'}
+]
+
+const books = [
+    {id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1},
+    {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
+    {id: 3, name: 'Harry Potter and the Goblet of Fire ', authorId: 1},
+    {id: 4, name: 'The Fellowship of the Ring', authorId: 2},
+    {id: 5, name: 'The Two Towers', authorId: 2},
+    {id: 6, name: 'The Return of the King', authorId: 2},
+    {id: 7, name: 'The way of Shadows', authorId: 3},
+    {id: 8, name: 'Beyound the Shadows', authorId: 3},
+]
+
+console.log(typeof(books[0]))
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        book: {
+            id: {type: GraphQLNonNull(GraphQLInt)},
+            name: {type: GraphQLNonNull(GraphQLString)},
+            authorId: {type: GraphQLNonNull(GraphQLInt)}
+        }
     })
 })
 
+const RootQueryType = new GraphQLObjectType({
+    name:'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of All Books',
+            resolve: () => books
+        }
+    }) 
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
+const AuthorType = GraphQLObjectType({
+    name: 'Author',
+    description: 'One author',
+    fields: () => ({
+        id: {type: GraphQLNonNull(GraphQLInt)},
+        name: {type: GraphQLNonNull(GraphQLString)}
+    })
+})
+
+const QueryAuthors = GraphQLObjectType({
+    name: 'QueryAuthors',
+    description: 'List of all representerd books authors',
+    fields: () => ({
+        authors: {
+            type: new CrapgQLList(AuthorType),
+            resolve: () => authors
+        }
+    })
+})
+
+const schemaAuthors = GraphQLSchema({
+    query: QueryAuthors
+})
+
 app.use('/graphql', graphqlHTTP({
-    schema,
+    schema,schemaAuthors,
     graphiql: true
 }))
 app.listen(5001, () => console.log('Server is running now...'))
