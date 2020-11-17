afe5c8216cf7fca66d21b876123188959c66ab44
naglec13 <naglec13@gmail.com>
2020-11-17 18:16:06 +0300

    fixed an error with custom BookType

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const { request } = require('express')
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const {
-    buildSchema, 
+    buildSchema,
     GraphQLObjectType, 
     GraphQLSchema, 
     GraphQLNonNull, 
@@ -10,24 +9,6 @@ const {
     GraphQLInt,
     GraphQLString} = require('graphql')
 const app = express()
-
-// const schema = buildSchema(`
-//     type Query{
-//         hello: String
-//     }
-// `)
-
-// const schema = new GraphQLSchema({
-//     query: new GraphQLObjectType({
-//         name:'HelloWorld',
-//         fields: () => ({
-//             message: {
-//                 type: GraphQLString,
-//                 resolve: () => 'Hello world from GraphQL'
-//             }
-//         })
-//     })
-// })
 
 const authors = [
     {id: 1, name: 'J. K. Rowling'},
@@ -37,13 +18,13 @@ const authors = [
 
 const books = [
     {id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1},
-    {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
-    {id: 3, name: 'Harry Potter and the Goblet of Fire ', authorId: 1},
-    {id: 4, name: 'The Fellowship of the Ring', authorId: 2},
-    {id: 5, name: 'The Two Towers', authorId: 2},
-    {id: 6, name: 'The Return of the King', authorId: 2},
-    {id: 7, name: 'The way of Shadows', authorId: 3},
-    {id: 8, name: 'Beyound the Shadows', authorId: 3},
+    // {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
+    // {id: 3, name: 'Harry Potter and the Goblet of Fire ', authorId: 1},
+    // {id: 4, name: 'The Fellowship of the Ring', authorId: 2},
+    // {id: 5, name: 'The Two Towers', authorId: 2},
+    // {id: 6, name: 'The Return of the King', authorId: 2},
+    // {id: 7, name: 'The way of Shadows', authorId: 3},
+    // {id: 8, name: 'Beyound the Shadows', authorId: 3},
 ]
 
 console.log(typeof(books[0]))
@@ -52,11 +33,9 @@ const BookType = new GraphQLObjectType({
     name: 'Book',
     description: 'This represents a book written by an author',
     fields: () => ({
-        book: {
             id: {type: GraphQLNonNull(GraphQLInt)},
             name: {type: GraphQLNonNull(GraphQLString)},
             authorId: {type: GraphQLNonNull(GraphQLInt)}
-        }
     })
 })
 
@@ -76,32 +55,8 @@ const schema = new GraphQLSchema({
     query: RootQueryType
 })
 
-const AuthorType = GraphQLObjectType({
-    name: 'Author',
-    description: 'One author',
-    fields: () => ({
-        id: {type: GraphQLNonNull(GraphQLInt)},
-        name: {type: GraphQLNonNull(GraphQLString)}
-    })
-})
-
-const QueryAuthors = GraphQLObjectType({
-    name: 'QueryAuthors',
-    description: 'List of all representerd books authors',
-    fields: () => ({
-        authors: {
-            type: new CrapgQLList(AuthorType),
-            resolve: () => authors
-        }
-    })
-})
-
-const schemaAuthors = GraphQLSchema({
-    query: QueryAuthors
-})
-
 app.use('/graphql', graphqlHTTP({
-    schema,schemaAuthors,
+    schema,
     graphiql: true
 }))
 app.listen(5001, () => console.log('Server is running now...'))
